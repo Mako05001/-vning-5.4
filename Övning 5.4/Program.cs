77e86600f26c6f178a6624b61fe4d7f8b2c00320
MAKSY <MAKSY@DESKTOP-6S4ESUP>
2023-01-26 19:49:08 +0100

    Add project files.

---
+++ Övning 5.4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hur många namn vill du skriva?");
+int S = int.Parse(Console.ReadLine());
+string[] N = new string[S];
+string F = N[0];
+Console.WriteLine("Skiv ett namn");
+N[0] = Console.ReadLine();
+for (int i =1; i < N.Length; i++)  {
+    Console.WriteLine("Skiv ett namn");
+    N[i] = Console.ReadLine();
+    if (N[i].CompareTo(N[0]) < 0)   {
+        F = N[i];
+    }
+}
+Console.WriteLine("Första namnet i bokstavsordning är " + F);
